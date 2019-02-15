a9c252506c902e9aeec25c5ccba6ec7c6bf8b401
patricktu2 <patricktu93@gmail.com>
2019-02-15 20:28:58 +0100

    file deleted

--- Sally_Sales_Chrome/js/uifix.js
+++
@@ -1,9 +0,0 @@
-
-// Remove trial header
-document.getElementsByClassName("trial-header")[0].style.display="none";
-// Remove call utility bar on bottom utilitybar
-document.getElementsByClassName("utilitybar")[0].style.display="none";
-// Remove Cenonis menu on top
-document.getElementsByClassName("analysis-menu")[0].style.display="none";
-// Remove tabs menu on bottom
-document.getElementsByClassName("tabs")[0].style.display="none";
